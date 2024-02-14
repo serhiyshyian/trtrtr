b8170a948cf9942082d0aae3fbd9e37e3f9b1a40
Serhiy <ser.shyian@gmail.com>
2024-02-14 15:37:54 +0200

    2version

--- src/main/java/org/example/SumCalculator.java
+++ src/main/java/org/example/SumCalculator.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class SumCalculator {
     public int sum(int n) {
-        if (n < 0) {
+        if (n <= 0) {
             throw new IllegalArgumentException("Parameter must be non-negative");
         }
         return n * (n + 1) / 2;
